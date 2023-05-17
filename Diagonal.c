646ce8b6bce3d66ecaf79ecf195c743c2fd4fd58
Víctor Suquilanda <131053641+Vik-tor39@users.noreply.github.com>
2023-05-17 16:01:56 -0500

    Version 3: Codigo completo

--- Diagonal.c
+++ Diagonal.c
@@ -1,26 +1,47 @@
-#include<stdio.h>
-int main(){
-    int fil,col;
-    int matriz[fil][col];
-    scanf("%d",&fil);
-    scanf("%d",&col);
-    for(int i=0;i<=fil;i++){
-        for(int j=0;j<=col;j++){
-            matriz[i][j]=0;
+#include <stdio.h>
+int main()
+{
+    int filas, columnas;
+    printf("Ingrese el numero de filas que desea para su matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el numero de columnas que desea para su matriz: ");
+    scanf("%d", &columnas);
+    int matriz[filas][columnas];
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz[i][j] = 0;
         }
     }
-    for(int i=0;i<=fil;i++){
-        for(int j=0;j<=col;j++){
-            if(i==j){
-                matriz[fil][col]=1;
-            }
-        }
-    }
-    for(int i=0;i<=fil;i++){
-        for(int j=0;j<=col;j++){
-            printf("%d",matriz[fil][col]);
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf("%d", matriz[i][j]);
         }
         printf("\n");
     }
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+        if (i==j)
+        {
+            matriz[i][j]=1;
+        }
+        
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf("%d", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
